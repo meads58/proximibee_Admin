93e1059213a156f17c3c247d330601093a72725c
meads58 <meads109@gmail.com>
2015-04-15 16:44:13 +0100

    only has_many will use destroy

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "1002x855>", :thumb => "100x100>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
-  belongs_to :beacon, dependent: :destroy
+  belongs_to :beacon
 
   def image_url
     image.url(:medium)
